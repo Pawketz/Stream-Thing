c42c9ca41d5d43ab7cbd488be03d8cd7d4377274
ItsRiprod <riprodgames@gmail.com>
2025-07-29 18:36:57 -0400

    Added to the obsStore singleton and fixed settings

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import {
   DESKTHING_EVENTS,
   SETTING_TYPES,
 } from "@deskthing/types";
-import { obsStore } from "./obsStore";
+import obsStore from "./obsStore";
 import { DeskThing } from '@deskthing/server';
 import { setupSettings } from './setupSettings';
 import './obsController'; // Integrate OBS controller backend
@@ -14,24 +14,26 @@ const startup = async () => {
 }
 
 // Create a singleton instance of obsStore
-const obsStoreInstance = new obsStore();
+// const obsStoreInstance = new obsStore();
 
 DeskThing.on(DESKTHING_EVENTS.SETTINGS, (settings) => {
   // Syncs the data with the server
   if (settings && settings.payload) {
     console.debug("Settings updating");
     // Ensure host is a string
-    obsStoreInstance.setHost(settings.payload.host.value?.toString() ?? "");
-    obsStoreInstance.setPort(Number(settings.payload.port.value));
-    obsStoreInstance.setPassword(settings.payload.password?.toString() ?? "");
+    obsStore.setHost(settings.payload.host.value?.toString() ?? "");
+    obsStore.setPort(Number(settings.payload.port.value));
+    obsStore.setPassword(settings.payload.password.value?.toString() ?? "");
     // You can also update port and password here if needed:
     // if (settings.payload.port !== undefined) obsStoreInstance.setPort(Number(settings.payload.port));
     // if (settings.payload.password !== undefined) obsStoreInstance.setPassword(settings.payload.password?.toString() ?? "");
+  } else {
+    console.warn("Settings payload is missing");
   }
 });
 
 const stop = () => {
-    DeskThing.send({ type: 'log', payload: 'Stopping app' });
+    DeskThing.send({ type: 'log', payload: 'Stopping app' }); // i see several things sending type: 'log' - if this is for normal logging, try just doing `console.log()` instead as it's treated the same way
 }
 
 const purge = () => {
@@ -40,6 +42,6 @@ const purge = () => {
 
 
 // Ideally, all of your code logic is held within the Startup function. Anything outside of this function wont reliably run
-DeskThing.on('start', startup);
-DeskThing.on('stop', stop);
-DeskThing.on('purge', purge);
+DeskThing.on(DESKTHING_EVENTS.START, startup);
+DeskThing.on(DESKTHING_EVENTS.STOP, stop);
+DeskThing.on(DESKTHING_EVENTS.PURGE, purge);
